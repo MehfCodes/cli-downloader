39e28988c6c5ffcf1a321325dd2f5d093fc71a9d
MehfCodes <mrmohammadehf@gmail.com>
2021-05-01 01:27:16 +0430

    add progress bar for downloading status

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 let http;
 const fs = require('fs');
-const link = process.argv[2];
-let pathFile = process.argv[3];
+const ProgressBar = require('./progressbar');
 
+const link =
+  'https://host2.rj-mw1.com/media/mp3/mp3-256/' +
+  process.argv[2].substring(process.argv[2].lastIndexOf('/') + 1) +
+  '.mp3';
+let pathFile = process.argv[3];
 if (link.startsWith('https')) {
   http = require('https');
 } else if (link.startsWith('http')) {
@@ -15,20 +19,31 @@ if (!pathFile) {
   process.exit(1);
 }
 
-http.get(link, (res) => {
-  let file;
-  const contentType = res.headers['content-type'];
-  const ext = contentType.split('/')[1];
-  let nameFile = `${link.substring(
-    link.lastIndexOf('/') + 1,
-    link.lastIndexOf('.')
-  )}-${Date.now()}.${ext}`;
-  if (!fs.existsSync(pathFile)) {
-    pathFile = `${__dirname}/${pathFile}`;
-    fs.mkdirSync(pathFile);
-  }
+http
+  .get(link, (res) => {
+    let file;
+    const contentType = res.headers['content-type'];
+    let ext = contentType.split('/')[1];
+    if (ext === 'mpeg') ext = 'mp3';
+    let nameFile = `${link.substring(
+      link.lastIndexOf('/') + 1,
+      link.lastIndexOf('.')
+    )}-${Date.now()}.${ext}`;
+    if (!fs.existsSync(pathFile)) {
+      pathFile = `${__dirname}/${pathFile}`;
+      fs.mkdirSync(pathFile);
+    }
 
-  const finalPath = `${pathFile}/${nameFile}`;
-  file = fs.createWriteStream(finalPath);
-  res.pipe(file);
-});
+    const finalPath = `${pathFile}/${nameFile}`;
+    file = fs.createWriteStream(finalPath);
+    const progressBar = new ProgressBar(res.headers['content-length']);
+    res
+      .on('data', (chunk) => {
+        progressBar.addChunk(chunk.length);
+        progressBar.showProgressBar();
+      })
+      .pipe(file);
+  })
+  .on('error', (err) => {
+    console.log(err);
+  });
---
+++ progressbar.js
@@ -0,0 +1,69 @@
+const process = require('process');
+const rdl = require('readline');
+const rl = rdl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+class ProgressBar {
+  constructor(totalSize) {
+    this.totalSize = totalSize;
+    this.percentage = 0;
+    this.downloaded = 0;
+    this.progressBarWidth = 20;
+    this.cursor = rl.cursor;
+    this.cursorForPercentage = 25;
+    this.drawLines();
+  }
+  addChunk(chunk) {
+    this.downloaded += chunk;
+  }
+  calcutePercentage() {
+    this.percentage = parseFloat(
+      ((this.downloaded / this.totalSize) * 100).toFixed(2)
+    );
+    return this.percentage;
+  }
+
+  drawLines() {
+    process.stdout.write('\x1B[?25l');
+    process.stdout.write('[');
+    for (let i = 0; i < 20; i++) {
+      process.stdout.write('\x1b[37m');
+      rl.write('.');
+    }
+    process.stdout.write(']');
+    this.cursor++;
+    rdl.cursorTo(process.stdout, this.cursor);
+  }
+  showPercentage() {
+    rdl.cursorTo(process.stdout, this.cursorForPercentage);
+    process.stdout.write(`${this.percentage}%`);
+    rdl.cursorTo(process.stdout, this.cursor);
+  }
+  showProgressBar() {
+    const points = Array.from(
+      { length: this.progressBarWidth + 1 },
+      (v, i) => i * 5
+    );
+
+    if (parseInt(this.calcutePercentage()) === points[this.cursor]) {
+      this.showPercentage();
+      process.stdout.write('\x1b[47m');
+      process.stdout.write('\x1b[8m');
+      rl.write('\u2588');
+      process.stdout.write('\x1b[0m');
+      this.cursor++;
+      if (this.cursor > this.progressBarWidth) {
+        process.stdout.write('\x1B[?25h');
+        process.stdout.clearLine();
+        rdl.cursorTo(process.stdout, 0);
+        process.stdout.write('download finished');
+        rl.close();
+        return;
+      }
+    }
+  }
+}
+
+module.exports = ProgressBar;
